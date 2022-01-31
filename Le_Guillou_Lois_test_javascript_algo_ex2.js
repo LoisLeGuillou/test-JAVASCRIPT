381b26a0d7d3222692f6063994fdf8042c0db575
LoisLeGuillou <97901849+LoisLeGuillou@users.noreply.github.com>
2022-01-31 12:14:27 +0100

    Add files via upload

--- Le_Guillou_Lois_test_javascript_algo_ex2.js
+++ Le_Guillou_Lois_test_javascript_algo_ex2.js
@@ -1,9 +1,15 @@
 function isDifferent(array) {
-    for (i=1 ; i<array.length ; i++){
-        if (array[i] == array[0]) {
-            return false;
-        } else {
-            return true;
+    var sortArray = array.sort((a,b)=>a-b);
+    var arraySorted = ['1','2','3','4','5','6','7','8','9'];
+    var isOK = 0;
+    for (var i=0; i<array.length; i++) {
+        if (sortArray[i] == arraySorted[i]) {
+           isOK+=1;
         }
+    }
+    if (isOK ==9){
+        return true;
+    } else {
+        return false;
     }
 }
